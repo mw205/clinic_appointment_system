7251c256826cdf721e0386cf45012d287b67b4ea
MostafaAbdelmajied <mostafa.abdo2029@gmail.com>
2026-04-20 16:18:11 +0200

    feat: add custom user model & doctor and patient profile models

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'appointments.apps.AppointmentsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
