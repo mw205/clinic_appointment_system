c3f4ce3e954bf4953686920441ff38f588de534b
MostafaAbdelmajied <mostafa.abdo2029@gmail.com>
2026-04-27 03:33:33 +0300

    chore: extend token ttl to 1 hour

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -107,7 +107,7 @@ REST_FRAMEWORK = {
 
 # Simple JWT configuration
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 
     "ROTATE_REFRESH_TOKENS": True,
