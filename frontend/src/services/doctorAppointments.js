6b52dee95ff09759ff72184b2903d3aeb0a32a03
MostafaAbdelmajied <mostafa.abdo2029@gmail.com>
2026-04-28 02:00:24 +0300

    feat: implement check in appointment flow

--- frontend/src/services/doctorAppointments.js
+++ frontend/src/services/doctorAppointments.js
@@ -5,3 +5,14 @@ export async function getAppointments(params = {})
   const res = await api.get('/appointments/doctor/appointments/', { params })
   return res.data
 }
+
+export async function getDailyQueue(params = {})
+{
+  const res = await api.get('/appointments/doctor/queue/', { params })
+  return res.data.data
+}
+
+export async function checkInAppointment(appointment_id)
+{
+  return await api.post(`/appointments/${appointment_id}/check_in/`)
+}
--- frontend/src/stores/DoctorAppointments.js
+++ frontend/src/stores/DoctorAppointments.js
@@ -7,13 +7,15 @@ export const useDoctorAppointmentsStore = defineStore('doctorAppointments', () =
   const dailyQueue = ref([]);
   const pendingRequests = ref([]);
   const loading = ref(false);
+  const actionStatus = ref(null)
+  const actionMessage = ref('')
 
   async function loadDailyQueue() {
     loading.value = true
 
     try {
-      dailyQueue.value = await doctorAppointmentsService.getAppointments({
-        status: 'confirmed',
+      dailyQueue.value = await doctorAppointmentsService.getDailyQueue({
+        // status: 'confirmed',
         // date: new Date().toISOString().slice(0,10)
       });
     }finally {
@@ -21,5 +23,16 @@ export const useDoctorAppointmentsStore = defineStore('doctorAppointments', () =
     }
   }
 
-  return { dailyQueue, pendingRequests, loading, loadDailyQueue };
+async function checkInAppointment(appointment_id) {
+  loading.value = true
+  try {
+    const response = await doctorAppointmentsService.checkInAppointment(appointment_id)
+    await loadDailyQueue()
+    return response.data
+  } finally {
+    loading.value = false
+  }
+}
+
+  return { dailyQueue, pendingRequests, loading, loadDailyQueue, checkInAppointment, actionStatus, actionMessage };
 })
